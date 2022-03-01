94a7418f658ec692d8a499e70ebb8a5dc044c5c8
Wendel Segadilha <wendelsegadilha99@gmail.com>
2022-03-01 20:34:23 -0300

    testando a primeira classe de dominio - Categoria

--- src/main/java/xyz/wendelsegadilha/cursonelioalves/resources/CategoriaResource.java
+++ src/main/java/xyz/wendelsegadilha/cursonelioalves/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package xyz.wendelsegadilha.cursonelioalves.resources;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import xyz.wendelsegadilha.cursonelioalves.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST funcionando!"; 
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		ArrayList<Categoria> lista = new ArrayList<>();
+		lista.addAll(Arrays.asList(cat1, cat2));
+		
+		return lista; 
 	}
 }
