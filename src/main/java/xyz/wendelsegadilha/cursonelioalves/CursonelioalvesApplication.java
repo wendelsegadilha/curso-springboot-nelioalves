3c269336e3fd1e21f195a47cf1e560137b353af9
Wendel Segadilha <wendelsegadilha99@gmail.com>
2022-03-01 18:22:17 -0300

    criacao do projeto

---
+++ src/main/java/xyz/wendelsegadilha/cursonelioalves/CursonelioalvesApplication.java
@@ -0,0 +1,13 @@
+package xyz.wendelsegadilha.cursonelioalves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursonelioalvesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursonelioalvesApplication.class, args);
+	}
+
+}
