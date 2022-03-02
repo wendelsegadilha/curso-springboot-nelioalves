ab003d9eba98fc21f2d1adf5af39c7a5572920a4
Wendel Segadilha <wendelsegadilha99@gmail.com>
2022-03-02 17:13:42 -0300

    criando operacao de instanciacao

--- src/main/java/xyz/wendelsegadilha/cursonelioalves/CursonelioalvesApplication.java
+++ src/main/java/xyz/wendelsegadilha/cursonelioalves/CursonelioalvesApplication.java
@@ -1,13 +1,32 @@
 package xyz.wendelsegadilha.cursonelioalves;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursonelioalvesApplication {
+import xyz.wendelsegadilha.cursonelioalves.domain.Categoria;
+import xyz.wendelsegadilha.cursonelioalves.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursonelioalvesApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursonelioalvesApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
