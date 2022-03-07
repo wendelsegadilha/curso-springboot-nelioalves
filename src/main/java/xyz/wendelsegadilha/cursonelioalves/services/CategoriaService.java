30ce264ab4d236e503fe7c161332efbbaf604820
Wendel Segadilha <wendelsegadilha99@gmail.com>
2022-03-07 09:45:58 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/xyz/wendelsegadilha/cursonelioalves/services/CategoriaService.java
+++ src/main/java/xyz/wendelsegadilha/cursonelioalves/services/CategoriaService.java
@@ -11,14 +11,18 @@ import xyz.wendelsegadilha.cursonelioalves.services.exceptions.ObjectNotFoundExc
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElseThrow(
-					() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName())
-				);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
 	}
 }
