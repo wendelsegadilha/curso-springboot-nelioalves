e8ab21db6ccc53ea5b781fa7574b7f2aaae65669
Wendel Segadilha <wendelsegadilha99@gmail.com>
2022-03-08 15:37:34 -0300

    Inserindo um novo Cliente com POST

--- src/main/java/xyz/wendelsegadilha/cursonelioalves/services/ClienteService.java
+++ src/main/java/xyz/wendelsegadilha/cursonelioalves/services/ClienteService.java
@@ -10,25 +10,39 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import xyz.wendelsegadilha.cursonelioalves.domain.Cidade;
 import xyz.wendelsegadilha.cursonelioalves.domain.Cliente;
+import xyz.wendelsegadilha.cursonelioalves.domain.Endereco;
+import xyz.wendelsegadilha.cursonelioalves.domain.enums.TipoCliente;
 import xyz.wendelsegadilha.cursonelioalves.dto.ClienteDTO;
+import xyz.wendelsegadilha.cursonelioalves.dto.ClienteNewDTO;
 import xyz.wendelsegadilha.cursonelioalves.repositories.ClienteRepository;
+import xyz.wendelsegadilha.cursonelioalves.repositories.EnderecoRepository;
 import xyz.wendelsegadilha.cursonelioalves.services.exceptions.DataIntegrityException;
 import xyz.wendelsegadilha.cursonelioalves.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class ClienteService {
-	
+
 	@Autowired
 	private ClienteRepository repo;
 	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
-		return obj.orElseThrow(
-					() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName())
-				);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
-	
+
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
+	}
+
 	public Cliente update(Cliente obj) {
 		Cliente newObj = find(obj.getId());
 		updateData(newObj, obj);
@@ -52,11 +66,26 @@ public class ClienteService {
 		PageRequest pageRequest = PageRequest.of(page, size, Direction.valueOf(direction), order);
 		return repo.findAll(pageRequest);
 	}
-	
+
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
 	}
-	
+
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cid, cli);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3() != null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
+	}
+
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
