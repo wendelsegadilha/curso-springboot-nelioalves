a86afb7400cea5fae4a97659f521afb8a45cd110
Wendel Segadilha <wendelsegadilha99@gmail.com>
2022-03-08 21:43:33 -0300

    Validaao customizada: email nao repetido na insercao de Cliente

--- src/main/java/xyz/wendelsegadilha/cursonelioalves/services/validation/ClienteInsertValidator.java
+++ src/main/java/xyz/wendelsegadilha/cursonelioalves/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import xyz.wendelsegadilha.cursonelioalves.domain.Cliente;
 import xyz.wendelsegadilha.cursonelioalves.domain.enums.TipoCliente;
 import xyz.wendelsegadilha.cursonelioalves.dto.ClienteNewDTO;
+import xyz.wendelsegadilha.cursonelioalves.repositories.ClienteRepository;
 import xyz.wendelsegadilha.cursonelioalves.resources.exception.FieldMessage;
 import xyz.wendelsegadilha.cursonelioalves.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -28,6 +36,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if (objDto.getTipo().equals(TipoCliente.PESSOA_JURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = clienteRepository.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 
 		for (FieldMessage e : list) {
