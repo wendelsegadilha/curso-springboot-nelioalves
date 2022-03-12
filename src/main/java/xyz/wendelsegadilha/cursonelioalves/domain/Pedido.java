8e455b12ac09d267e467ba902c10c7d3e99b9d80
Wendel Segadilha <wendelsegadilha99@gmail.com>
2022-03-12 15:43:18 -0300

    Implementando toString de Pedido

--- src/main/java/xyz/wendelsegadilha/cursonelioalves/domain/Pedido.java
+++ src/main/java/xyz/wendelsegadilha/cursonelioalves/domain/Pedido.java
@@ -1,8 +1,11 @@
 package xyz.wendelsegadilha.cursonelioalves.domain;
 
 import java.io.Serializable;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.Locale;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -54,7 +57,7 @@ public class Pedido implements Serializable {
 
 	public double getValorTotal() {
 		double soma = 0.0;
-		for(ItemPedido ip : itens) {
+		for (ItemPedido ip : itens) {
 			soma += ip.getSubTotal();
 		}
 		return soma;
@@ -131,6 +134,28 @@ public class Pedido implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	@Override
+	public String toString() {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+		StringBuilder builder = new StringBuilder();
+		builder.append("Pedido número: ");
+		builder.append(getId());
+		builder.append(", Data/Hora: ");
+		builder.append(sdf.format(getInstante()));
+		builder.append(", Cliente: ");
+		builder.append(getCliente().getNome());
+		builder.append(", Situação do pagamento: ");
+		builder.append(getPagamento().getEstado().getDescricao());
+		builder.append("\nDetalhes:\n");
+		for (ItemPedido ip : itens) {
+			builder.append(ip.toString());
+		}
+		builder.append("Valor total: ");
+		builder.append(nf.format(getValorTotal()));
+		return builder.toString();
 	}
 
 }
